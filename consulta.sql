157d1fc91dd13aee49e0079a5d0eb9ecf8073950
guzmanjuanpablo2503 <83968426+guzmanjuanpablo2503@users.noreply.github.com>
2021-08-04 13:27:06 -0500

    Update consulta.sql

--- consulta.sql
+++ consulta.sql
@@ -1,1 +1,17 @@
+INSERT INTO mascotas (raza, nombre, vacunas, especie, id_residente)
+VALUES ('Bulldog', 'cronos', 'si', 'canino', '4') , ('pitbull', 'riot', 'si', 'canino', '8');
 
+INSERT INTO parqueadero (num_parqueadero, vacunas, matricula, area)
+VALUES ('34', '64rb', '2m') , ('67', '6rb', '1m');
+
+INSERT INTO vehiculo (placa, modelo, marca)
+VALUES ('rgg 356', 'txl', 'toyota') , ('gyt 597', 'logan', 'renault');
+
+INSERT INTO residente (nombre_residente, apellido_residente, edad, id_representante, lugar_trabajo, eps, telefono)
+VALUES ('chin chan', 'chung', '34', '65', 'cali', 'coomeva', '345245555') , ('jhony', 'rauw', '24', '22', 'cali', 'sura', '34567555');
+
+INSERT INTO residente (tipo_id_propietario, num_identificacion, tel_propietario, ocupacion, edad, nom_propietario)
+VALUES ('cc', '1110286612', '3002223467', 'ingeniero', '35', 'michael') , ('cc', '3667542', '30357467', 'arquitecto', '25', 'jose');
+
+INSERT INTO apartamento (matricula_apartamento, num_apartamento, bloque)
+VALUES ('456', '567', 'no') , ('874', '933', 'si');
